00cb5aac0b4ba9b5ba14d1c698755c89b3e2aba8
adducjpcole <cjpcole@addu.edu.ph>
2026-03-22 21:10:25 +0800

    fix: bad redirect urls

--- util/@requireAuth.js
+++ util/@requireAuth.js
@@ -1,7 +1,8 @@
 import * as auth from '@/auth.js';
 
+const loginUrl = new URL('../login/', import.meta.url);
 if (!auth.getCurrentUser()) {
-  document.location.href = '/login/';
+  document.location.href = loginUrl.href;
 } else {
   document.documentElement.style.visibility = 'visible';
 }
--- util/@requireUnauth.js
+++ util/@requireUnauth.js
@@ -1,7 +1,8 @@
 import * as auth from '@/auth.js';
 
+const homeUrl = new URL('../', import.meta.url);
 if (auth.getCurrentUser()) {
-  document.location.href = '/';
+  document.location.href = homeUrl.href;
 } else {
   document.documentElement.style.visibility = 'visible';
 }
